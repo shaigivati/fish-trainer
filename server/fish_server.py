c9b3f6724eec81854d6dad2c1214b5f12dd473d2
shaigivati <shaigiv@post.bgu.ac.il>
2018-02-20 13:16:50 +0200

    added GUI

--- server/fish_server.py
+++ server/fish_server.py
@@ -61,11 +61,13 @@ def main_server():
 	TCP_PORT = 5008
 	BUFFER_SIZE = 1024  #was 20-  Normally 1024, but we want fast response
 
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+	print s
+	s.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind((TCP_IP, TCP_PORT))
+	s.listen(1)
+	print s
 	if (flag_first_time):
-		s.bind((TCP_IP, TCP_PORT))
-		s.listen(1)
+
 		flag_first_time=False
 		print ("Server is up and waiting for connections")
 		i_progress_count=0
