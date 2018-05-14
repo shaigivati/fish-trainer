f13a8cbae5e3b89d9951cc2ad56741b2c8ff52c0
shaigivati <shaigiv@post.bgu.ac.il>
2018-05-14 19:43:14 +0300

    update

--- server/GUI_Server/MainWindowApp.py
+++ server/GUI_Server/MainWindowApp.py
@@ -62,6 +62,7 @@ class MainWindowApp(Tkinter.Tk):
         self.Pin['2RD'] = ConfigSectionMap("Tank")['tank 2 right direction pin']
 
         feed = feeder.Feeder({self.Pin_en[1], self.Pin['1L'], self.Pin['1R'], self.Pin_en[2], self.Pin['2L'], self.Pin['2R'], self.Pin['2LD'], self.Pin['2RD']})
+        add_step = feed.add_program_step(1, 'wait', 1)
         add_step = feed.add_program_step(9, 'left', (360+180), 0.1)
         add_step = feed.add_program_step(10, 'wait', 0.2)
         add_step = feed.add_program_step(11, 'right', 60 , 0.3)
