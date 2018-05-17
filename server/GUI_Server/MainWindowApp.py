ffcaa054e0131aa8a68f7ef3353cea024374d7fd
shaigivati <shaigiv@post.bgu.ac.il>
2018-05-17 14:37:26 +0300

    update

--- server/GUI_Server/MainWindowApp.py
+++ server/GUI_Server/MainWindowApp.py
@@ -63,8 +63,8 @@ class MainWindowApp(Tkinter.Tk):
 
         feed = feeder.Feeder({self.Pin_en[1], self.Pin['1L'], self.Pin['1R'], self.Pin_en[2], self.Pin['2L'], self.Pin['2R'], self.Pin['2LD'], self.Pin['2RD']})
 
-        add_step = feed.add_program_step(1, 'left', 180, 4000, 10)
-        add_step = feed.add_program_step(2, 'wait', 10)
+        add_step = feed.add_program_step(1, 'left', 180, 800, 4)
+        add_step = feed.add_program_step(2, 'wait', 4)
         add_step = feed.add_program_step(3, 'left', 180, 2, 1)
 
         #add_step = feed.add_program_step(9, 'wait', 3)
